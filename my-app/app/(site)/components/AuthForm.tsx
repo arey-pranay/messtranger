9d09b1aadce7ac362d6e24a754b55c73fdc54e43
arey-pranay <pranayparikh2004@gmail.com>
2023-11-01 13:10:34 +0530

    Setting up basic functions and states for AuthForm

---
+++ my-app/app/(site)/components/AuthForm.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+  //using the imported form..
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    if (variant === "REGISTER") {
+      //Axios Register
+    }
+    if (variant === "LOGIN") {
+      //NextAuth SignIn
+    }
+  };
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+    //NextAuth Social Sign In
+  };
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
