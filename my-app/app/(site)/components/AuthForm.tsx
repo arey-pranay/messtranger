aefe18c68370f11f4c656ae0a324d0b6309ff700
arey-pranay <pranayparikh2004@gmail.com>
2023-11-02 16:13:51 +0530

    Made basic form and a input component also...

--- my-app/app/(site)/components/AuthForm.tsx
+++ my-app/app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Input from "@/app/components/inputs/input";
 import { useCallback, useState } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 type Variant = "LOGIN" | "REGISTER";
@@ -39,7 +40,15 @@ const AuthForm = () => {
     setIsLoading(true);
     //NextAuth Social Sign In
   };
-  return <div>AuthForm</div>;
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <Input />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
